4ab3570899b5838f62350f03a17fbe27e1a02584
Marcus Weiner <marcus.weiner@gmail.com>
2022-03-25 12:51:04 +0100

    Peer with R14

--- netconfig.sh
+++ netconfig.sh
@@ -11,3 +11,8 @@ ip tunnel add tun2 mode gre local 45.151.166.3 remote 178.13.251.114
 ip addr add 10.129.0.4/31 dev tun2
 ip addr add fdcb:aa6b:5532:3::1/64 dev tun2
 ip link set up dev tun2
+
+ip tunnel add tun4 mode gre local 45.151.166.3 remote 178.15.126.23
+ip addr add 10.129.0.14/31 dev tun4
+ip addr add fdcb:aa6b:5532:8::1/64 dev tun4
+ip link set up dev tun4
